a9ff469d6af9382816ce80e0f369212e8fb1fdbf
AndresSarabia <j.andresst@gmail.com>
2026-04-05 00:17:24 -0400

    Se configuro adminlte para el proyecto

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombre',45); 
+            $table->string('apellidos',64);
+            $table->string('ci')->nullable();
+            $table->string('sexo', 10)->nullable();
+            $table->string('cargo')->nullable();
+            $table->string('telefono')->nullable();
+            $table->string('direccion')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('perfil'); 
+            $table->string('estado',1)->default('1');
             $table->rememberToken();
             $table->timestamps();
         });
